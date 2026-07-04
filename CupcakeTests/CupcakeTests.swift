4f7f30943a0f567aaadfb9acc7a1d099e85b04c9
Toan Phung <toan.phung@proteo.info>
2026-07-04 11:57:55 +0100

    Initial Commit

---
+++ CupcakeTests/CupcakeTests.swift
@@ -0,0 +1,18 @@
+//
+//  CupcakeTests.swift
+//  CupcakeTests
+//
+//  Created by Toan Phung on 04/07/2026.
+//
+
+import Testing
+
+struct CupcakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
